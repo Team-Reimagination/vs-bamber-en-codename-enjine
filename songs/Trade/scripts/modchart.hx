2a6309557eb6068af0ea15caf191975f0672cbd4
C4rr13 M4th <126361810+C4rr13-M4th@users.noreply.github.com>
2023-08-26 20:18:22 +0100

    part 2.2

---
+++ songs/Trade/scripts/modchart.hx
@@ -0,0 +1,34 @@
+function onNoteCreation(event) {
+	if (event.note.strumLine == playerStrums && !pixelNotesForBF) return;
+	if (event.note.strumLine == cpuStrums && !pixelNotesForDad) return;
+
+	event.cancel();
+
+	var note = event.note;
+	if (event.note.isSustainNote) {
+		note.loadGraphic(Paths.image('game/notes/trade'), true, 7, 6);
+		note.animation.add("hold", [event.strumID]);
+		note.animation.add("holdend", [4 + event.strumID]);
+	} else {
+		note.loadGraphic(Paths.image('game/notes/trade'), true, 17, 17);
+		note.animation.add("scroll", [4 + event.strumID]);
+	}
+	note.scale.set(daPixelZoom, daPixelZoom);
+	note.updateHitbox();
+}
+
+function onStrumCreation(event) {
+	if (event.player == 1 && !pixelNotesForBF) return;
+	if (event.player == 0 && !pixelNotesForDad) return;
+
+	event.cancel();
+
+	var strum = event.strum;
+	strum.loadGraphic(Paths.image('game/notes/trade'), true, 17, 17);
+	strum.animation.add("static", [event.strumID]);
+	strum.animation.add("pressed", [4 + event.strumID, 8 + event.strumID], 12, false);
+	strum.animation.add("confirm", [12 + event.strumID, 16 + event.strumID], 24, false);
+
+	strum.scale.set(daPixelZoom, daPixelZoom);
+	strum.updateHitbox();
+}
