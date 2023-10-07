c92db4ad94cc03ddd8355e4fd231a60813e7c80c
C4rr13 <dumpufffnf@gmail.com>
2023-10-07 20:34:50 +0100

    Add new files via upload

--- data/charts/Davey's Nightmare.hx
+++ data/charts/Davey's Nightmare.hx
@@ -5,7 +5,6 @@ var bloom = new CustomShader('bloom');
 var scanline = new CustomShader('scanLines');
 var vignette = new CustomShader('vignette');
 var cooler = new CustomShader('sketchShader');
-
 var elapsedShader:Float = 0;
 var grainStrength:Float = 16;
 var actualGrainStrength:Float = 16;
@@ -15,41 +14,50 @@ var oppositeHealth:Float = 1;
 var hdr:Float = 1.5;
 var time:Float;
 
-if(["astray", "facsimile", "placeholder", "test footage"].contains(curSong)){
-    function postCreate(){
-        for(a in [cooler, bloom, shader, scanline]){camHUD.addShader(a);}
-        for(b in [cooler, bloom, vignette]){camGame.addShader(b);}
-        bloom.data.hDRthingy.value = [1.5];
-        shader.data.strength.value = [35];
-        vignette.data.size.value = [1.2];
-        scanline.data.opacity.value = [0.3];
-        for(a in [boyfriend, iconP2, healthBar, healthBarBG]){
-           a.alpha = 0;
-        }
-        FlxG.state.forEachOfType(FlxText, text -> text.visible = false);
-    }
+if ([
+	"astray",
+	"facsimile",
+	"placeholder",
+	"test footage",
+	"astray old",
+	"astray oldest",
+	"placeholder old"
+].contains(curSong)) {
+	function postCreate() {
+		for (a in [cooler, bloom, shader, scanline]) {
+			camHUD.addShader(a);
+		}
+		for (b in [cooler, bloom, vignette]) {
+			camGame.addShader(b);
+		}
+		bloom.data.hDRthingy.value = [1];
+		shader.data.strength.value = [20];
+		vignette.data.size.value = [1.2];
+		scanline.data.opacity.value = [0.3];
+		for (a in [boyfriend, iconP2, healthBar, healthBarBG]) {
+			a.alpha = 0;
+		}
+		FlxG.state.forEachOfType(FlxText, text -> text.visible = false);
+	}
 
-    function postUpdate(elapsed){
-        camFollow.y = dad.getMidpoint().y + 50;
-        camFollow.x = dad.getMidpoint().x + 20;
+	function postUpdate(elapsed) {
+		camFollow.y = dad.getMidpoint().y + 50;
+		camFollow.x = dad.getMidpoint().x + 20;
 
-        oppositeHealth = (2.2 - PlayState.health);
+		oppositeHealth = (2.2 - PlayState.health);
 
-        chromaticaAbber = FlxMath.lerp(chromaticaAbber, 0.1, 0.02);
-        bloom.data.chromatic.value = [chromaticaAbber];
-        if (curBeat != tempBeat)
-            {
-                chromaticaAbber = 1;
-                tempBeat = curBeat;
-            }
-        hdr = 1.5 - (PlayState.misses / 20);
-        bloom.data.hDRthingy.value = [hdr];
-        elapsedShader += Std.parseFloat(elapsed);
-        shader.data.iTime.value = [elapsedShader];
-        grainStrength = oppositeHealth * 47;
-        actualGrainStrength = FlxMath.lerp(actualGrainStrength, grainStrength, 0.01);
-        shader.data.strength.value = [actualGrainStrength];
+		chromaticaAbber = FlxMath.lerp(chromaticaAbber, 0.1, 0.02);
+		bloom.data.chromatic.value = [chromaticaAbber];
+		if (curBeat != tempBeat) {
+			chromaticaAbber = 1;
+			tempBeat = curBeat;
+		}
+		hdr = 1.5 - (PlayState.misses / 20);
+		bloom.data.hDRthingy.value = [hdr];
+		elapsedShader += Std.parseFloat(elapsed + 1);
+		shader.data.iTime.value = [elapsedShader];
+		shader.data.strength.value = [1];
 
-        scanline.data.opacity.value = [oppositeHealth/6];
-    }
+		scanline.data.opacity.value = [oppositeHealth / 6];
+	}
 }
--- data/charts/MinimalStuff.hx
+++ data/charts/MinimalStuff.hx
@@ -21,14 +21,22 @@ var songFonts = [
 ];
 
 var barColors = [
-	"bf" => 0xFF00CCFF "bamber" => 0xFF71C419 "davey" => 0xFF4087CE "davey-shred" => 0xFF4087CE "ronnie" => 0xFFFFF600 "swindled" => 0xFFEE0000
-	"bambi" => 0xFF25BF37 "trade-bamber" => 0xFF71C419 "trade-davey" => 0xFF4087CE "null" => 0xFFB9CEB9
+	"bf" => 0xFF00CCFF
+	"bamber" => 0xFF71C419
+	"davey" => 0xFF4087CE
+	"davey-shred" => 0xFF4087CE
+	"ronnie" => 0xFFFFF600
+	"swindled" => 0xFFEE0000
+	"bambi" => 0xFF25BF37
+	"trade-bamber" => 0xFF71C419
+	"trade-davey" => 0xFF4087CE
+	"null" => 0xFFB9CEB9
+	"bamber-sans-old" => 0xFF00A1FF
 ];
 
 var array = [];
 
 function postCreate() {
-	player.cpu = true;
 	if (songFonts[curSong] != null) {
 		FlxG.state.forEachOfType(FlxText, text -> text.font = Paths.font(songFonts[curSong]));
 	}
@@ -40,31 +48,4 @@ function postCreate() {
 	}
 	healthBar.createFilledBar(p1color, p2color);
 	FlxG.state.forEachOfType(FlxText, text -> text.color = p1color);
-	if (curSong == "coop") {
-		newIcon1 = new HealthIcon("bf-coop", true);
-		newIcon2 = new HealthIcon("davey-coop", false);
-	}
-	for (a in [newIcon1, newIcon2]) {
-		add(a);
-	}
-}
-
-function postUpdate() {
-	newIcon1.x = iconP1.x;
-	newIcon1.y = iconP1.y;
-}
-
-if (!["astray", "facsimile", "placeholder", "test footage"].contains(curSong)) {
-	function postUpdate() {
-		switch (strumLines.members[curCameraTarget].characters[0].getAnimName()) {
-			case "singLEFT":
-				camFollow.x -= 20;
-			case "singDOWN":
-				camFollow.y += 20;
-			case "singUP":
-				camFollow.y -= 20;
-			case "singRIGHT":
-				camFollow.x += 20;
-		}
-	}
 }
