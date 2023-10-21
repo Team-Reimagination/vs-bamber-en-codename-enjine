217260938c6aa0b5eebb9c7ffb666688391d22c2
C4rr13 <dumpufffnf@gmail.com>
2023-10-21 19:22:14 +0100

    Portraits

--- data/states/FreeplayState.hx
+++ data/states/FreeplayState.hx
@@ -1,27 +1,49 @@
-/*var categories = [
-	"Week Bamber" => ["Yield", "Cornaholic", "Harvest"],
-	"Week Davey" => ["Synthwheel", "Yard", "Coop"],
-	"Week Ronnie & Boris" => ["Ron be like", "Bob be like", "Fortnite Duos"],
-	"Joke Songs" => ["Judgement Farm", "Judgement Farm 2", "Generations", "Memeing", "Yeld"],
-	"Bonus Songs" => ["Multiversus", "Swindled", "Trade", "Blusterous Day"],
-	"Collab Songs" => ["Call-Bamber", "Deathbattle", "H2O"],
-	"Crossover Songs" => ["Corn N Roll", "Screencast"],
-	"Legacy/Old Content" => [
-		"Yield In-Dev", "Yield Demo", "Yield Oldest", "Cornaholic Demo", "Cornaholic Oldest", "Harvest Demo", "Harvest Oldest", "Yield Seezee",
-		"Cornaholic Erect", "Harvest Chill", "Fortnite Duos Old", "Swindled Old", "Best Farmers Forever", "Judgement Farm Old", "Godzilla", "Call Bamber Old",
-		"Astray Old", "Astray Oldest", "Placeholder Old"
-	], // I fear this might be the BIGGEST category in v3
-	// you were right
-	"DAVEY'S NIGHTMARE" => ["Astray", "Facsimile", "Placeholder"]
-];
-
-function postCreate() {
-	var curCat = categories["Legacy/Old Content"];
-	for (a in 0...songs.length) {
-		if (!curCat.contains(songs[a].name)) {
-			songs.remove(songs[a]);
-			grpSongs.remove(grpSongs[a]);
+import flixel.FlxCamera;
+import flixel.tweens.FlxTween;
+var newCam = new FlxCamera(-150, 0);
+var prevchar:String;
+var time:Float;
+function postCreate(){
+    // Text rescaling
+    newCam.bgColor = 0;
+    for (a in [grpSongs.members, iconArray]){
+        for(b in a) b.cameras = [newCam];
+    }
+    FlxG.cameras.add(newCam, false);
+    newCam.zoom = 0.8;
+    // Portraits
+    add(portrait = new FlxSprite(700, -50));
+    portrait.scale.set(0.7, 0.7);
+    portrait.antialiasing = true;
+    updatePortrait();
+}
+function postUpdate(elapsed){
+    time += elapsed;
+    FlxG.stage.window.title = "Vs Bamber And Davey V3 | Freeplay Menu | Currently Selecting: " + songs[curSelected].name;
+    if(FlxG.keys.justPressed.UP || FlxG.keys.justPressed.DOWN){
+        updatePortrait();
+    }
+    for (song in grpSongs.members) {
+		if (song.text.toLowerCase() == "swindled" || song.text.toLowerCase() == "swindled old"){
+			song.angle = FlxG.random.float(-13, 13);
+			var sine = Math.sin(95 * time) * 2;
+			var shake = FlxG.random.float(0, 45) * 0.1;
+			song.x += ((shake * sine)) * (elapsed * 85);
+			var sineY = Math.sin(125 * time) * 2;
+			var shakeY = FlxG.random.float(0, 60) * 0.1;
+			song.y += ((shakeY * sineY)) * (elapsed * 85);
 		}
 	}
+    FlxG.camera.angle = grpSongs.members[curSelected].angle/5;
+    newCam.angle = grpSongs.members[curSelected].angle/5;
 }
-*/
+function updatePortrait(){
+    FlxTween.globalManager.cancelTweensOf(portrait);
+    portrait.x = 700;
+    if(Assets.exists(Paths.image("menus/Portraits/" + songs[curSelected].icon))){
+        portrait.loadGraphic(Paths.image("menus/Portraits/" + songs[curSelected].icon));
+    } else {
+        portrait.loadGraphic(Paths.image("menus/Portraits/the_placeholder"));
+    }
+    FlxTween.tween(portrait, { x: 500 }, 1, {ease: FlxEase.elasticOut});
+}
