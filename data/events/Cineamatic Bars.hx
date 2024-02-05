4b16c87b5fad5b88dd544ee4f229363a515a8ece
Frakist <90983918+Frakist@users.noreply.github.com>
2024-02-05 20:23:03 +0200

    wip cineamatic bars

--- data/events/Cineamatic Bars.hx
+++ data/events/Cineamatic Bars.hx
@@ -1,50 +1,40 @@
-var cinematicBarTween1:FlxTween = null;
-var cinematicBarTween2:FlxTween = null;
-
 var cinematicBar1:FunkinSprite = null;
 var cinematicBar2:FunkinSprite = null;
 
-function create() {
-    for (i in 0...2) {
-        var cinematicBar:FunkinSprite = new FunkinSprite().makeSolid(1, 1, 0xFF000000);
-        cinematicBar.scrollFactor.set(0, 0);
-        cinematicBar.zoomFactor = 0;
-        add(cinematicBar);
+var cinematicAngle:Float = 0;
+var cinematicLength:Float = 0;
 
-        cinematicBar.scale.set(FlxG.width, 0);
-        cinematicBar.updateHitbox();
+function postCreate() {
+    cinematicBar1 = new FunkinSprite(0, -360).makeGraphic(1280, 360, 0xFF000000);
+    cinematicBar1.zoomFactor = 0;
+    cinematicBar1.scrollFactor.set(0, 0);
+    add(cinematicBar1);
 
-        if (i == 1) cinematicBar2 = cinematicBar;
-        else cinematicBar1 = cinematicBar;
-    }
+    cinematicBar2 = new FunkinSprite(0, 720).makeGraphic(1280, 360, 0xFF000000);
+    cinematicBar2.zoomFactor = 0;
+    cinematicBar2.scrollFactor.set(0, 0);
+    add(cinematicBar2);
 }
 
-function update(elapsed:Float) {
-    if (cinematicBarTween2 != null && cinematicBarTween2.active && cinematicBarTween1 != null && cinematicBarTween1.active)
-        for (bar in [cinematicBar1, cinematicBar2]) bar.updateHitbox();
-    cinematicBar2.y = FlxG.height - cinematicBar2.height;
+function update() {
+    cinematicBar1.origin.y = 720 - cinematicLength;
+    cinematicBar1.angle = cinematicAngle;
+    //cinematicBar1.y = cinematicLength - 360;
+
+    cinematicBar2.origin.y = -360 + cinematicLength;
+    cinematicBar2.angle = cinematicAngle;
+    //cinematicBar2.y = -cinematicLength + 720;
 }
 
 function onEvent(eventEvent) {
     var params:Array = eventEvent.event.params;
     if (eventEvent.event.name == "Cineamatic Bars") {
         if (params[0] == false)
-            for (bar in [cinematicBar1, cinematicBar2]) {
-                bar.scale.y = (FlxG.height/2) * params[1];
-                bar.updateHitbox();
-            }
+            cinematicLength = (FlxG.height/2) * params[1];
         else {
-            for (twn in [cinematicBarTween1, cinematicBarTween2])
-                if (twn != null) twn.cancel();
-            
-            var flxease:String = params[3] + (params[3] == "linear" ? "" : params[4]);
-
-            for (bar in [cinematicBar1, cinematicBar2]) {
-                var tween:FlxTween = FlxTween.tween(bar.scale, {y: (FlxG.height/2) * params[1]}, ((Conductor.crochet / 4) / 1000) * params[2], {ease: Reflect.field(FlxEase, flxease)});
-                if (bar == cinematicBar1) cinematicBarTween1 = tween;
-                else cinematicBarTween2 = tween;
-            }
-            
+            var flxease:String = params[4] + (params[4] == "linear" ? "" : params[5]);
+            FlxTween.num(cinematicLength, (FlxG.height/2) * params[1], ((Conductor.crochet / 4) / 1000) * params[3], {ease: Reflect.field(FlxEase, flxease)}, function(num) cinematicLength = num);
+            FlxTween.num(cinematicAngle, params[2], ((Conductor.crochet / 4) / 1000) * params[3], {ease: Reflect.field(FlxEase, flxease)}, function(num) cinematicAngle = num);
         }
     }
 }
