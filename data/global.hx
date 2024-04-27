d1ea5c1436dbea3d694781a4f87cbb9114b58006
Verwex <34553484+Verwex@users.noreply.github.com>
2024-04-27 21:57:09 +0200

    all assets for the options substate

--- data/global.hx
+++ data/global.hx
@@ -22,11 +22,59 @@ function new() {
     WindowUtils.set_prefix('Bamber & Davey Vol. 2.5 | ');
     FlxG.save.bind('BamberAndDavey', 'TeamReimagination'); //I found out that mod options use regular saves instead of a save in the Options class for example
 
-    if (FlxG.save.data.options == null) FlxG.save.data.options = {}; //will be rewritten ONCE the mod is finished
+    if (FlxG.save.data.options == null) FlxG.save.data.options = {};
+
+    //MOD SPECIFIC OPTIONS, DEFAULT ONES SHOULD BE INCLUDED TOO
+    //Video Options
+    if (FlxG.save.data.options.resolution == null) FlxG.save.data.options.resolution = [1280, 720];
+    if (FlxG.save.data.options.fullscreen == null) FlxG.save.data.options.fullscreen = false; 
+    if (FlxG.save.data.options.borderless == null) FlxG.save.data.options.borderless = false;
+    if (FlxG.save.data.options.brightness == null) FlxG.save.data.options.brightness = 50;
+    if (FlxG.save.data.options.gamma == null) FlxG.save.data.options.gamma = 50;
+
+    //Sound options
+    //Master Volume - FlxG.volume
     if (FlxG.save.data.options.musicVolume == null) FlxG.save.data.options.musicVolume = 1; 
     if (FlxG.save.data.options.sfxVolume == null) FlxG.save.data.options.sfxVolume = 1;
     if (FlxG.save.data.options.voiceVolume == null) FlxG.save.data.options.voiceVolume = 1;
+    if (FlxG.save.data.options.missSounds == null) FlxG.save.data.options.missSounds = true;
+    if (FlxG.save.data.options.copyrightBypass == null) FlxG.save.data.options.copyrightBypass = false;
+    if (FlxG.save.data.options.subtitles == null) FlxG.save.data.options.subtitles = true;
 
+    //Appearance Options
+    if (FlxG.save.data.options.flashingLights == null) FlxG.save.data.options.flashingLights = true;
+    if (FlxG.save.data.options.shaders == null) FlxG.save.data.options.shaders = 'all';
+    if (FlxG.save.data.options.botplayUI == null) FlxG.save.data.options.botplayUI = true;
+    if (FlxG.save.data.options.bgBlur == null) FlxG.save.data.options.bgBlur = 0;
+    if (FlxG.save.data.options.bgDim == null) FlxG.save.data.options.bgDim = 0;
+    if (FlxG.save.data.options.rapidCam == null) FlxG.save.data.options.rapidCam = true;
+    if (FlxG.save.data.options.breakTime == null) FlxG.save.data.options.breakTime = true;
+    if (FlxG.save.data.options.timeBar == null) FlxG.save.data.options.timeBar = true;
+    if (FlxG.save.data.options.comboPosPercent == 0) FlxG.save.data.options.comboPosPercent = 0;
+    if (FlxG.save.data.options.cinematicBars == null) FlxG.save.data.options.cinematicBars = true;
+    if (FlxG.save.data.options.healthIcons == null) FlxG.save.data.options.healthIcons = true;
+    if (FlxG.save.data.options.songCredits == null) FlxG.save.data.options.songCredits = true;
+    if (FlxG.save.data.options.stampKeybinds == null) FlxG.save.data.options.stampKeybinds = false;
+
+    //Notes Options
+    if (FlxG.save.data.options.noteskin == null) FlxG.save.data.options.noteskin = 'arrows';
+    if (FlxG.save.data.options.noteScale == null) FlxG.save.data.options.noteScale = 1;
+    if (FlxG.save.data.options.noteColors == null) FlxG.save.data.options.noteColors = [0xFFC24B99, 0xFF00FFFF, 0xFF12FA05, 0xFFF9393F];
+
+    //Control Options
+    //will have to be reserved elsewhere
+
+    //Gameplay Options
+    if (FlxG.save.data.options.modcharts == null) FlxG.save.data.options.modcharts = 'always';
+    if (FlxG.save.data.options.dialogue == null) FlxG.save.data.options.dialogue = [true, true, false]; //Story Mode, Playlists, Freeplay
+    if (FlxG.save.data.options.scrollSpeed == null) FlxG.save.data.options.scrollSpeed = false;
+    if (FlxG.save.data.options.scrollSpeed_Speed == null) FlxG.save.data.options.scrollSpeed_Speed = 3;
+    if (FlxG.save.data.options.pauseCountdown == null) FlxG.save.data.options.pauseCountdown = true;
+    if (FlxG.save.data.options.skipGameOver == null) FlxG.save.data.options.skipGameOver = 'off';
+    if (FlxG.save.data.options.skipSongIntro == null) FlxG.save.data.options.skipSongIntro = false;
+    if (FlxG.save.data.options.middlescroll == null) FlxG.save.data.options.middlescroll = false;
+
+    //Game Statistics
     if (FlxG.save.data.gameStats == null) FlxG.save.data.gameStats = {};
     if (FlxG.save.data.gameStats.discoveries == null) FlxG.save.data.gameStats.discoveries = {
         "Bamber's Farm": false,
@@ -115,4 +163,8 @@ public static function clearClickables() {
 
 public static function getClickables() {
     return clickableObjects;
+}
+
+public static function playBamberMenuSound(type) {
+    return FlxG.sound.play(Paths.sound('menuSounds/'+type), getVolume(1, 'sfx'));
 }
