8b6b4a1ba059d9de14b833c1eb588cb3e9b02927
Verwex <34553484+Verwex@users.noreply.github.com>
2024-08-21 23:50:27 +0200

    REAL SHIT with floating characters

--- songs/cameraMove.hx
+++ songs/cameraMove.hx
@@ -1,21 +1,27 @@
 import flixel.math.FlxPoint;
 import flixel.FlxObject;
-var oldPosition:FlxPoint = FlxPoint.get();
 var driftAmount:Int = 30;
 var otherCamFollow:FlxObject = new FlxObject();
 var whichStrumline:Int = 0;
 var oldCamTarget;
+var oldCamPos:FlxObject = new FlxObject();
 
 public var cameraEasing:FlxEase = FlxEase.quartInOut;
 public var cameraTime:Float = 1;
+public var cameraTween;
 function onCameraMove(e) {
-    e.cancel();
-
-    if ((oldPosition.x != e.position.x || oldPosition.y != e.position.y) && curCameraTarget != oldCamTarget) {
+    if (curCameraTarget != oldCamTarget && (cameraTween == null || (cameraTween != null && !cameraTween.active))) {
         FlxTween.cancelTweensOf(camFollow);
-        FlxTween.tween(camFollow, {x: e.position.x, y: e.position.y}, cameraTime, {ease: cameraEasing});
-        oldPosition = FlxPoint.get(e.position.x, e.position.y);
+        cameraTween = FlxTween.tween(camFollow, {x: e.position.x, y: e.position.y}, cameraTime, {ease: cameraEasing});
+        oldCamPos.setPosition(camFollow.x, camFollow.y);
         trace(cameraEasing);
+    }
+
+    if (cameraTween != null && cameraTween.active) { 
+        e.cancel();
+
+        cameraTween._propertyInfos[0].range = e.position.x - oldCamPos.x;
+        cameraTween._propertyInfos[1].range = e.position.y - oldCamPos.y;
     }
 
     oldCamTarget = curCameraTarget;
